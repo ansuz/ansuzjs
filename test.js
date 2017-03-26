3d57be551c54870eddfbe262394e00ca8875870a
ansuz <ansuz@transitiontech.ca>
2017-03-26 12:58:50 +0200

    remove buster dependency

--- test.js
+++ test.js
@@ -1,256 +1,249 @@
 var $=require("./ansuz.js");
-var buster=require("buster");
-var assert=buster.assert;
-var refute=buster.refute;
+var Assert = require("assert");
+var Refute = function (x) { Assert.ok(!x); };
 
-buster.testCase("ansuzjs", {
-  "tests":function(){
+var Equal = function (a, b, m) {
+    Assert.deepStrictEqual(a, b, m);
+};
 
-    assert.equals($.find({
-        a:{b:{c:{d:5}}}
-    }, ['a', 'b', 'c', 'd']), 5);
+Equal($.find({
+    a:{b:{c:{d:5}}}
+}, ['a', 'b', 'c', 'd']), 5);
 
-    refute($.find({
-        a: {b: {c: 5}}
-    }, ['a', 'd']));
+Refute($.find({
+    a: {b: {c: 5}}
+}, ['a', 'd']));
 
-    // incrementing 3 by two yields 5
-    assert.equals($.count({x: 3}, 'x', 2), 5);
+// incrementing 3 by two yields 5
+Equal($.count({x: 3}, 'x', 2), 5);
 
-    // incrementing 7 by negative two yields 5
-    assert.equals($.count({x:7}, 'x', -2), 5);
+// incrementing 7 by negative two yields 5
+Equal($.count({x:7}, 'x', -2), 5);
 
-    // incrementing x by the default 1 will yield 5
-    assert.equals($.count({x: 4}, 'x'), 5);
+// incrementing x by the default 1 will yield 5
+Equal($.count({x: 4}, 'x'), 5);
 
-    // undefined maps return false
-    assert.equals($.count(null, '5'), false);
+// undefined maps return false
+Equal($.count(null, '5'), false);
 
-    // count works on anything that has attributes
-    assert.equals($.count([1,2,3], '1'), 3);
+// count works on anything that has attributes
+Equal($.count([1,2,3], '1'), 3);
 
-    assert.equals( // 2 cubed is 8 
-      $.fix1(Math.pow,2)(3)
-      ,8
-    );
+Equal( // 2 cubed is 8 
+  $.fix1(Math.pow,2)(3)
+  ,8
+);
 
-    assert.equals(
-      $.fix2(Math.pow,3)(2)
-      ,8
-    );
+Equal(
+  $.fix2(Math.pow,3)(2)
+  ,8
+);
 
-    assert.equals(
-      $.isArray([0,1,2])
-      ,true
-    );
+Equal(
+  $.isArray([0,1,2])
+  ,true
+);
 
-    assert.equals(
-      $.fixN(function(a,b,c){return c;},5,2)(1,2)
-      ,5
-    );
+Equal(
+  $.fixN(function(a,b,c){return c;},5,2)(1,2)
+  ,5
+);
 
-    assert.equals(
-      $.negate($.fix1($.is,4))(3)
-      ,true
-    );
+Equal(
+  $.negate($.fix1($.is,4))(3)
+  ,true
+);
 
-    assert.equals(
-      $.compose($.negate,$.negate($.fix1($.is,4)))(4)
-      ,true
-    );
+Equal(
+  $.compose($.negate,$.negate($.fix1($.is,4)))(4)
+  ,true
+);
 
-    assert.equals(
-      $.sum($.range(4))
-      ,10
-    );
+Equal(
+  $.sum($.range(4))
+  ,10
+);
 
-    assert.equals(
-      $.sum([])
-      ,0
-    );
+Equal(
+  $.sum([])
+  ,0
+);
 
-    assert.equals(
-      $.nullArray(3)
-      ,[null,null,null]
-    );
+Equal(
+  $.nullArray(3)
+  ,[null,null,null]
+);
 
-    assert.equals(
-      $.flatten([[1],[2],[3]])
-      ,$.range(1,3)
-    );
+Equal(
+  $.flatten([[1],[2],[3]])
+  ,$.range(1,3)
+);
 
-    assert.equals(
-      $.flatten($.carte(function(x,y){return x*y;},[1,2],[3,4]))
-      ,[3,4,6,8]
-    );
+Equal(
+  $.flatten($.carte(function(x,y){return x*y;},[1,2],[3,4]))
+  ,[3,4,6,8]
+);
 
-    assert.equals(
-      $.flatten($.carteSquare(function(x,y){return x+y;},[0,1]))
-      ,[0,1,1,2]
-    );
+Equal(
+  $.flatten($.carteSquare(function(x,y){return x+y;},[0,1]))
+  ,[0,1,1,2]
+);
 
-    assert.equals(
-      $.flatten([])
-      ,[]
-    );
+Equal(
+  $.flatten([])
+  ,[]
+);
 
-    assert.equals(
-      $.unique($.shuffle([0,1,2,3]))
-      ,['0','1','2','3']
-    );
+Equal(
+  $.unique($.shuffle([0,1,2,3]))
+  ,['0','1','2','3']
+);
 
-    assert.equals(
-      function(x){return x>-1 && x<4;}($.die(4))
-      ,true
-    );
+Equal(
+  function(x){return x>-1 && x<4;}($.die(4))
+  ,true
+);
 
-    // comb
+// comb
 
-    assert.equals(
-      $.cut([0,1,2,3],2)
-      ,[2,3,0,1]
-    );
+Equal(
+  $.cut([0,1,2,3],2)
+  ,[2,3,0,1]
+);
 
-    assert.equals(
-      $.vals({x:5,y:6})
-      ,[5,6]
-    );
+Equal(
+  $.vals({x:5,y:6})
+  ,[5,6]
+);
 
-    assert.equals(
-      $.keys({a:1,b:true})
-      ,['a','b']
-    );
+Equal(
+  $.keys({a:1,b:true})
+  ,['a','b']
+);
 
-    assert.equals(
-      $.clone({a:1,b:2})
-      ,{a:1,b:2}
-    );
+Equal(
+  $.clone({a:1,b:2})
+  ,{a:1,b:2}
+);
 
-    assert.equals(
-      $.vals($.merge({a:1,b:2},{b:3},function(x,y){return x*y;}))
-      ,[1,6]
-    );
+Equal(
+  $.vals($.merge({a:1,b:2},{b:3},function(x,y){return x*y;}))
+  ,[1,6]
+);
 
-    assert.equals(
-      $.stdDev([2,4,4,4,5,5,7,9])
-      ,2
-    );
+Equal(
+  $.stdDev([2,4,4,4,5,5,7,9])
+  ,2
+);
 
-    assert.equals(
-      $.first($.forget(function(x){return x*x;}),5)
-      ,[0,1,4,9,16]
-    );
+Equal(
+  $.first($.forget(function(x){return x*x;}),5)
+  ,[0,1,4,9,16]
+);
 
-    assert.equals(
-      $.listall($.either([5,7,2,4]))
-      ,[5,7,2,4]
-    );
+Equal(
+  $.listall($.either([5,7,2,4]))
+  ,[5,7,2,4]
+);
 
-    assert.equals(
-      $.first($.stateful(function(c){
-        return c.i=c.i*2;
-      },{i:1}),5)
-      ,[2,4,8,16,32]
-    );
+Equal(
+  $.first($.stateful(function(c){
+    return c.i=c.i*2;
+  },{i:1}),5)
+  ,[2,4,8,16,32]
+);
 
-    assert.equals(
-      $.splice("pewpew",3,"pew")
-      ,"pewpewpew"
-    );
+Equal(
+  $.splice("pewpew",3,"pew")
+  ,"pewpewpew"
+);
 
-    assert.equals(
-      $.swap("pew{BANG}pew",{"{BANG}":'pew'})
-      ,'pewpewpew'
-    );
+Equal(
+  $.swap("pew{BANG}pew",{"{BANG}":'pew'})
+  ,'pewpewpew'
+);
 
-    // ngraph
-    assert.equals(
-      function(x){return $.exists(x,$.choose(x));}([0,1,2,3,4,5])
-      ,true
-    );
+// ngraph
+Equal(
+  function(x){return $.exists(x,$.choose(x));}([0,1,2,3,4,5])
+  ,true
+);
 
-    assert.equals(
-        $.exists({a:1,b:2,c:3},2),
-        true
-    );
+Equal(
+    $.exists({a:1,b:2,c:3},2),
+    true
+);
 
-    refute.equals(
-        $.exists({a:1,b:2,c:3},5),
-        true
-    );
+Refute($.exists({a:1,b:2,c:3},5));
 
-    assert.equals(
-        5,
-        $.least([5,6,7])
-    );
+Equal(
+    5,
+    $.least([5,6,7])
+);
 
-    refute($.least());
-    refute($.least([]));
+Refute($.least());
+Refute($.least([]));
 
-    assert.equals(
-        7,
-        $.most([5,6,7])
-    );
+Equal(
+    7,
+    $.most([5,6,7])
+);
 
-    refute($.most());
-    refute($.most([]));
+Refute($.most());
+Refute($.most([]));
 
 
+// functions that still need to be tested...
 
+// weighted
+// schedule
+// memo
+// cycle
+// fcycle
+// fail
+// done
+// all
+// filter
+// cons
+// chain
+// combinatorial
 
-    // functions that still need to be tested...
+Equal(
+  $.intersection([0,1,2,3],[2,3,4,5])
+  ,[2,3]
+);
 
-    // weighted
-    // schedule
-    // memo
-    // cycle
-    // fcycle
-    // fail
-    // done
-    // all
-    // filter
-    // cons
-    // chain
-    // combinatorial
+Equal(
+  $.difference([0,1,2,3],[2,3,4,5])
+  ,[0,1,4,5]
+);
 
-    assert.equals(
-      $.intersection([0,1,2,3],[2,3,4,5])
-      ,[2,3]
-    );
+Assert($.superset([0,1,2],[1,2]));
+Refute($.superset([0],[1]));
 
-    assert.equals(
-      $.difference([0,1,2,3],[2,3,4,5])
-      ,[0,1,4,5]
-    );
+Assert($.subset([1,2],[0,1,2,3,4]));
 
-    assert($.superset([0,1,2],[1,2]));
-    refute($.superset([0],[1]));
+Assert($.every([1,2,3]));
+Refute($.every([0,1,2]));
+Assert($.every([15,1.5,0x00], function (n) {
+    return !isNaN(n);
+}));
 
-    assert($.subset([1,2],[0,1,2,3,4]));
+Refute($.some({
+    x: 5,
+    y: 7,
+    z: 9
+}, function (v, k) {
+    return v > 10;
+}));
 
-    assert($.every([1,2,3]));
-    refute($.every([0,1,2]));
-    assert($.every([15,1.5,0x00], function (n) {
-        return !isNaN(n);
-    }));
+Assert($.some([
+    12, 13, 14
+], function (v, k) {
+    return k < 10;
+}));
 
-    refute($.some({
-        x: 5,
-        y: 7,
-        z: 9
-    }, function (v, k) {
-        return v > 10;
-    }));
+Equal(
+    [3, 5, 1].sort($.invert($.subtract)),
+    [5, 3, 1]);
 
-    assert($.some([
-        12, 13, 14
-    ], function (v, k) {
-        return k < 10;
-    }));
-
-    assert.equals(
-        [3, 5, 1].sort($.invert($.subtract)),
-        [5, 3, 1]);
-
-  }
-});
